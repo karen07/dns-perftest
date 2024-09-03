ce0168f8fd600dd69357aac9059f09410b56599f
Karen <karen0734@gmail.com>
2024-09-03 13:40:46 +0300

    New version

---
+++ include/perftest.h
@@ -0,0 +1,29 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <limits.h>
+#include <poll.h>
+#include <pthread.h>
+#include <search.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <linux/limits.h>
+
+#define PACKET_MAX_SIZE 1500
+
+typedef struct dns_header {
+    uint16_t id;
+    uint16_t flags;
+    uint16_t quest;
+    uint16_t ans;
+    uint16_t auth;
+    uint16_t add;
+} __attribute__((packed)) dns_header_t;
+
+typedef struct end_name {
+    uint16_t type;
+    uint16_t class;
+} __attribute__((packed)) end_name_t;
--- src/perftest.c
+++ src/perftest.c
@@ -1,69 +1,44 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <limits.h>
-#include <poll.h>
-#include <pthread.h>
-#include <search.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <unistd.h>
+#include "perftest.h"
 
-typedef struct dns_header {
-    uint16_t id;
-    uint16_t flags;
-    uint16_t quest;
-    uint16_t ans;
-    uint16_t auth;
-    uint16_t add;
-} __attribute__((packed)) dns_header_t;
+FILE *fp;
 
-typedef struct end_name {
-    uint16_t type;
-    uint16_t class;
-} __attribute__((packed)) end_name_t;
+uint32_t dns_ip;
+uint16_t dns_port;
 
-int main(void)
+uint32_t listen_ip;
+uint16_t listen_port;
+
+uint32_t rps;
+
+int32_t is_domains_file_path;
+char domains_file_path[PATH_MAX];
+
+int32_t sended;
+int32_t readed;
+
+double coeff = 1;
+
+struct sockaddr_in repeater_addr, dns_addr;
+int repeater_socket;
+
+void print_help()
 {
-    FILE *fp = fopen("top.csv", "r");
-    if (!fp) {
-        printf("Error opening file top.csv\n");
-        return 0;
-    }
+    printf("Commands:\n"
+           "-file /example.txt            Domains file path\n"
+           "-DNS_IP 0.0.0.0               DNS IP\n"
+           "-DNS_port 00                  DNS port\n"
+           "-listen_IP 0.0.0.0            Listen IP\n"
+           "-listen_port 0000             Listen port\n"
+           "-RPS 00000                    Request per second\n");
+    exit(EXIT_FAILURE);
+}
 
-    struct sockaddr_in repeater_addr, dns_addr;
-
-    repeater_addr.sin_family = AF_INET;
-    repeater_addr.sin_port = htons(5052);
-    repeater_addr.sin_addr.s_addr = inet_addr("0.0.0.0");
-
-    dns_addr.sin_family = AF_INET;
-    dns_addr.sin_port = htons(5053);
-    dns_addr.sin_addr.s_addr = inet_addr("192.168.1.1");
-
-    int repeater_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (repeater_socket < 0) {
-        printf("Error:Error while creating socket %s\n", strerror(errno));
-        return 0;
-    }
-
-    if (bind(repeater_socket, (struct sockaddr *)&repeater_addr, sizeof(repeater_addr)) < 0) {
-        printf("Error:Couldn't bind to the port %s\n", strerror(errno));
-        return 0;
-    }
-
-    char packet[10000], line_buf[10000];
+void *send_dns(__attribute__((unused)) void *arg)
+{
+    char packet[PACKET_MAX_SIZE], line_buf[PACKET_MAX_SIZE];
     int line_count = 0;
 
     while (fscanf(fp, "%s", line_buf) != EOF) {
-        if (line_count % 10000 == 0) {
-            time_t now = time(NULL);
-            struct tm *tm_struct = localtime(&now);
-            printf("\nSended %d %d:%d\n", line_count, tm_struct->tm_min, tm_struct->tm_sec);
-        }
-
         line_count++;
 
         dns_header_t *header = (dns_header_t *)packet;
@@ -95,17 +70,201 @@ int main(void)
         end_name->type = htons(1);
         end_name->class = htons(1);
 
-        usleep(2000);
-
         if (sendto(repeater_socket, packet, 12 + k + 5, 0, (struct sockaddr *)&dns_addr,
                    sizeof(dns_addr)) < 0) {
             printf("Error:Can't send %s\n", strerror(errno));
         }
+
+        sended = line_count;
+
+        usleep(1000000 / rps / coeff);
     }
 
-    time_t now = time(NULL);
-    struct tm *tm_struct = localtime(&now);
-    printf("\nSended %d %d:%d\n", line_count, tm_struct->tm_min, tm_struct->tm_sec);
+    return NULL;
+}
+
+void *read_dns(__attribute__((unused)) void *arg)
+{
+    int32_t receive_msg_len = 0;
+    char receive_msg[PACKET_MAX_SIZE];
+
+    struct sockaddr_in receive_DNS_addr;
+    uint32_t receive_DNS_addr_length = sizeof(receive_DNS_addr);
+
+    while (1) {
+        receive_msg_len = recvfrom(repeater_socket, receive_msg, PACKET_MAX_SIZE, 0,
+                                   (struct sockaddr *)&receive_DNS_addr, &receive_DNS_addr_length);
+
+        if (receive_msg_len > 0) {
+            readed++;
+        }
+    }
+
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("\nDNS perftest started\n");
+
+    for (int i = 1; i < argc; i++) {
+        if (!strcmp(argv[i], "-file")) {
+            if (i != argc - 1) {
+                printf("Get urls from file %s\n", argv[i + 1]);
+                if (strlen(argv[i + 1]) < PATH_MAX) {
+                    is_domains_file_path = 1;
+                    strcpy(domains_file_path, argv[i + 1]);
+                }
+                i++;
+            }
+            continue;
+        }
+        if (!strcmp(argv[i], "-DNS_IP")) {
+            if (i != argc - 1) {
+                printf("DNS IP %s\n", argv[i + 1]);
+                if (strlen(argv[i + 1]) < INET_ADDRSTRLEN) {
+                    dns_ip = inet_addr(argv[i + 1]);
+                }
+                i++;
+            }
+            continue;
+        }
+        if (!strcmp(argv[i], "-DNS_port")) {
+            if (i != argc - 1) {
+                printf("DNS port %s\n", argv[i + 1]);
+                sscanf(argv[i + 1], "%hu", &dns_port);
+                i++;
+            }
+            continue;
+        }
+        if (!strcmp(argv[i], "-listen_IP")) {
+            if (i != argc - 1) {
+                printf("Listen IP %s\n", argv[i + 1]);
+                if (strlen(argv[i + 1]) < INET_ADDRSTRLEN) {
+                    listen_ip = inet_addr(argv[i + 1]);
+                }
+                i++;
+            }
+            continue;
+        }
+        if (!strcmp(argv[i], "-listen_port")) {
+            if (i != argc - 1) {
+                printf("Listen port %s\n", argv[i + 1]);
+                sscanf(argv[i + 1], "%hu", &listen_port);
+                i++;
+            }
+            continue;
+        }
+        if (!strcmp(argv[i], "-RPS")) {
+            if (i != argc - 1) {
+                printf("RPS %s\n", argv[i + 1]);
+                sscanf(argv[i + 1], "%u", &rps);
+                i++;
+            }
+            continue;
+        }
+        printf("Unknown command %s\n", argv[i]);
+        print_help();
+    }
+
+    if (!is_domains_file_path) {
+        printf("Programm need domains file path\n");
+        print_help();
+    }
+
+    if (dns_ip == 0) {
+        printf("Programm need DNS IP\n");
+        print_help();
+    }
+
+    if (dns_port == 0) {
+        printf("Programm need DNS port\n");
+        print_help();
+    }
+
+    if (listen_ip == 0) {
+        printf("Programm need listen IP\n");
+        print_help();
+    }
+
+    if (listen_port == 0) {
+        printf("Programm need listen port\n");
+        print_help();
+    }
+
+    if (rps == 0) {
+        printf("Programm need rps\n");
+        print_help();
+    }
+
+    printf("\n");
+
+    fp = fopen(domains_file_path, "r");
+    if (!fp) {
+        printf("Error opening file %s\n", domains_file_path);
+        return 0;
+    }
+
+    repeater_addr.sin_family = AF_INET;
+    repeater_addr.sin_port = htons(listen_port);
+    repeater_addr.sin_addr.s_addr = listen_ip;
+
+    dns_addr.sin_family = AF_INET;
+    dns_addr.sin_port = htons(dns_port);
+    dns_addr.sin_addr.s_addr = dns_ip;
+
+    repeater_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (repeater_socket < 0) {
+        printf("Error:Error while creating socket %s\n", strerror(errno));
+        return 0;
+    }
+
+    if (bind(repeater_socket, (struct sockaddr *)&repeater_addr, sizeof(repeater_addr)) < 0) {
+        printf("Error:Couldn't bind to the port %s\n", strerror(errno));
+        return 0;
+    }
+
+    pthread_t send_thread;
+    if (pthread_create(&send_thread, NULL, send_dns, NULL)) {
+        printf("Can't create send_thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_detach(send_thread)) {
+        printf("Can't detach send_thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t read_thread;
+    if (pthread_create(&read_thread, NULL, read_dns, NULL)) {
+        printf("Can't create read_thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_detach(read_thread)) {
+        printf("Can't detach read_thread\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int32_t sended_old = 0;
+    int32_t readed_old = 0;
+
+    printf("Min:Sec Sended Readed\n");
+    while (1) {
+        sleep(1);
+
+        time_t now = time(NULL);
+        struct tm *tm_struct = localtime(&now);
+        printf("%d:%d %d %d\n", tm_struct->tm_min, tm_struct->tm_sec, sended - sended_old,
+               readed - readed_old);
+
+        coeff = (1.0 * rps) / (sended - sended_old);
+
+        printf("%lf\n", coeff);
+
+        sended_old = sended;
+        readed_old = readed;
+    }
 
     return 0;
 }
